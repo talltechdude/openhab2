884317b796f5f093ac7140318c4af30c0db4744b
Scott Linton <talltechdude@gmail.com>
2016-03-09 18:50:37 +1100

    Updated latest Network Thermostat connection stability detection
    Signed-off-by: Scott Linton <talltechdude@gmail.com>
    (github: talltechdude)

--- addons/binding/org.openhab.binding.networkthermostat/src/main/java/org/openhab/binding/networkthermostat/handler/NetworkThermostatHandler.java
+++ addons/binding/org.openhab.binding.networkthermostat/src/main/java/org/openhab/binding/networkthermostat/handler/NetworkThermostatHandler.java
@@ -17,10 +17,12 @@ import java.math.BigDecimal;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 import org.eclipse.smarthome.config.core.Configuration;
+import org.eclipse.smarthome.core.common.ThreadPoolManager;
 import org.eclipse.smarthome.core.library.types.DecimalType;
 import org.eclipse.smarthome.core.library.types.OnOffType;
 import org.eclipse.smarthome.core.library.types.StringType;
@@ -44,7 +46,7 @@ import org.slf4j.LoggerFactory;
  */
 public class NetworkThermostatHandler extends BaseThingHandler {
 
-    private Logger logger = LoggerFactory.getLogger(NetworkThermostatHandler.class);
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private Socket socket;
     private InetAddress ipAddress;
@@ -55,6 +57,8 @@ public class NetworkThermostatHandler extends BaseThingHandler {
     private static final int DEFAULT_REFRESH_INTERVAL = 60;
     private ScheduledFuture<?> pollingJob;
 
+    private ExecutorService threadPool = ThreadPoolManager.getPool(this.getClass().getSimpleName());
+
     private Runnable pollingRunnable = new Runnable() {
 
         @Override
@@ -62,7 +66,35 @@ public class NetworkThermostatHandler extends BaseThingHandler {
             try {
                 updateThermostatStatus();
             } catch (Exception e) {
-                logger.debug("Exception during poll : {}", e);
+                logger.debug("Exception during poll: {}", e);
+            }
+        }
+    };
+
+    private Runnable thermostatReader = new Thread() {
+
+        @Override
+        public void run() {
+            while (!isInterrupted()) {
+                try {
+                    if (socket == null || !socket.isConnected() || socket.isClosed()) {
+                        connect();
+                    }
+                    String line = in.readLine();
+                    if (line != null) {
+                        processResponse(line);
+                        continue;
+                    } else {
+                        close();
+                    }
+                } catch (Exception e) {
+
+                } finally {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+                    }
+                }
             }
         }
     };
@@ -71,71 +103,117 @@ public class NetworkThermostatHandler extends BaseThingHandler {
         super(thing);
     }
 
-    protected void updateThermostatStatus() {
-        String result = sendCommand("RAS1");
-        logger.trace("RAS1: {}", result);
-        // Indoor Temp, Outdoor Temp, Mode, Fan, Override, Recovery, Cool Setpoint, Heat Setpoint, Status, Stages
-        String[] values = result.substring(5).split(",");
-        updateState(new ChannelUID(getThing().getUID(), CHANNEL_INDOOR_TEMP),
-                new DecimalType(Double.parseDouble(values[0])));
-        if (!values[1].equals("NA")) {
-            updateState(new ChannelUID(getThing().getUID(), CHANNEL_OUTDOOR_TEMP),
-                    new DecimalType(Double.parseDouble(values[1])));
-        } else {
-            updateState(new ChannelUID(getThing().getUID(), CHANNEL_OUTDOOR_TEMP), new DecimalType(0));
+    private void processResponse(String response) {
+        try {
+            logger.debug("Response received from thermostat {}: {}", getThing().getUID(), response);
+            int sep = response.indexOf(":");
+            String command = response.substring(0, sep);
+            String data = response.substring(sep + 1);
+            if (command.equals("RAS1")) {
+                String[] values = data.split(",");
+                // Indoor Temp, Outdoor Temp, Mode, Fan, Override, Recovery, Cool Setpoint, Heat Setpoint, Status,
+                // Stages
+                updateState(getThing().getChannel(CHANNEL_INDOOR_TEMP).getUID(),
+                        new DecimalType(Double.parseDouble(values[0])));
+                if (!values[1].equals("NA")) {
+                    updateState(getThing().getChannel(CHANNEL_OUTDOOR_TEMP).getUID(),
+                            new DecimalType(Double.parseDouble(values[1])));
+                } else {
+                    updateState(getThing().getChannel(CHANNEL_OUTDOOR_TEMP).getUID(), new DecimalType(0));
+                }
+                updateState(getThing().getChannel(CHANNEL_MODE).getUID(), new StringType(values[2]));
+                updateState(getThing().getChannel(CHANNEL_FAN_MODE).getUID(), new StringType(values[3].substring(4)));
+                updateState(getThing().getChannel(CHANNEL_OVERRIDE).getUID(),
+                        "YES".equals(values[4]) ? OnOffType.ON : OnOffType.OFF);
+                updateState(getThing().getChannel(CHANNEL_COOL_SETPOINT).getUID(),
+                        new DecimalType(Double.parseDouble(values[6])));
+                updateState(getThing().getChannel(CHANNEL_HEAT_SETPOINT).getUID(),
+                        new DecimalType(Double.parseDouble(values[7])));
+                updateState(getThing().getChannel(CHANNEL_COMPRESSOR_MODE).getUID(), new StringType(values[8]));
+                updateState(getThing().getChannel(CHANNEL_COMPRESSOR_STAGE).getUID(), new StringType(values[9]));
+            } else if (command.equals("RAOS1")) {
+                // RAOS1:15-10-15,17:27:53,0:00,0:0:00,TH3,DISABLED
+                String[] values = data.split(",");
+                String override[] = values[2].split(":");
+                updateState(getThing().getChannel(CHANNEL_OVERRIDE_TIMER).getUID(),
+                        new DecimalType(Integer.parseInt(override[0]) * 60 + Integer.parseInt(override[1])));
+            } else if (command.equals("RAAS1")) {
+                // RAAS1:YES,NO,NO,NO,NO,YES,NO,NO,NO,NO,NO,YES,NO,NO,NO,NO
+                String[] values = data.split(",");
+                updateState(getThing().getChannel(CHANNEL_LED1).getUID(),
+                        "YES".equals(values[14]) ? OnOffType.ON : OnOffType.OFF);
+                updateState(getThing().getChannel(CHANNEL_LED2).getUID(),
+                        "YES".equals(values[15]) ? OnOffType.ON : OnOffType.OFF);
+            }
+        } catch (Exception e) {
+            logger.error("Failed to parse response: {} - {}", response, e.getMessage());
         }
-        updateState(new ChannelUID(getThing().getUID(), CHANNEL_MODE), new StringType(values[2]));
-        updateState(new ChannelUID(getThing().getUID(), CHANNEL_FAN_MODE), new StringType(values[3].substring(4)));
-        updateState(new ChannelUID(getThing().getUID(), CHANNEL_OVERRIDE),
-                "YES".equals(values[4]) ? OnOffType.ON : OnOffType.OFF);
-
-        updateState(new ChannelUID(getThing().getUID(), CHANNEL_COOL_SETPOINT),
-                new DecimalType(Double.parseDouble(values[6])));
-        updateState(new ChannelUID(getThing().getUID(), CHANNEL_HEAT_SETPOINT),
-                new DecimalType(Double.parseDouble(values[7])));
-        updateState(new ChannelUID(getThing().getUID(), CHANNEL_COMPRESSOR_MODE), new StringType(values[8]));
-        updateState(new ChannelUID(getThing().getUID(), CHANNEL_COMPRESSOR_STAGE), new StringType(values[9]));
-
-        result = sendCommand("RAOS1"); // RAOS1:15-10-15,17:27:53,0:00,0:0:00,TH3,DISABLED
-        logger.trace("RAOS1: {}", result);
-        values = result.substring(6).split(",");
-        String override[] = values[2].split(":");
-        updateState(new ChannelUID(getThing().getUID(), CHANNEL_OVERRIDE_TIMER),
-                new DecimalType(Integer.parseInt(override[0]) * 60 + Integer.parseInt(override[1])));
-
-        result = sendCommand("RAAS1"); // RAAS1:YES,NO,NO,NO,NO,YES,NO,NO,NO,NO,NO,YES,NO,NO,NO,NO
-        logger.trace("RAAS1: {}", result);
-        values = result.substring(6).split(",");
-        updateState(new ChannelUID(getThing().getUID(), CHANNEL_LED1),
-                "YES".equals(values[14]) ? OnOffType.ON : OnOffType.OFF);
-        updateState(new ChannelUID(getThing().getUID(), CHANNEL_LED2),
-                "YES".equals(values[15]) ? OnOffType.ON : OnOffType.OFF);
-
-        /*
-         * <channel id="cool_setpoint" typeId="setpoint"/>
-         * <channel id="heat_setpoint" typeId="setpoint"/>
-         * <channel id="mode" typeId="mode"/>
-         * <channel id="indoor_temp" typeId="current_temp"/>
-         * <channel id="outdoor_temp" typeId="current_temp"/>
-         * <channel id="compressor_mode" typeId="compressor_mode"/>
-         * <channel id="compressor_stage" typeId="compressor_stage"/>
-         */
-
-        // TODO Auto-generated method stub
-
     }
+
+    private void updateThermostatStatus() {
+        if (getThing().getStatus().equals(ThingStatus.ONLINE)) {
+            sendCommand("RAS1");
+            sendCommand("RAOS1");
+            sendCommand("RAAS1");
+        }
+    }
+
+    // protected void updateThermostatStatus2() {
+    // String result = sendCommand("RAS1");
+    // logger.trace("RAS1: {}", result);
+    // // Indoor Temp, Outdoor Temp, Mode, Fan, Override, Recovery, Cool Setpoint, Heat Setpoint, Status, Stages
+    // String[] values = result.substring(5).split(",");
+    // updateState(new ChannelUID(getThing().getUID(), CHANNEL_INDOOR_TEMP),
+    // new DecimalType(Double.parseDouble(values[0])));
+    // if (!values[1].equals("NA")) {
+    // updateState(new ChannelUID(getThing().getUID(), CHANNEL_OUTDOOR_TEMP),
+    // new DecimalType(Double.parseDouble(values[1])));
+    // } else {
+    // updateState(new ChannelUID(getThing().getUID(), CHANNEL_OUTDOOR_TEMP), new DecimalType(0));
+    // }
+    // updateState(new ChannelUID(getThing().getUID(), CHANNEL_MODE), new StringType(values[2]));
+    // updateState(new ChannelUID(getThing().getUID(), CHANNEL_FAN_MODE), new StringType(values[3].substring(4)));
+    // updateState(new ChannelUID(getThing().getUID(), CHANNEL_OVERRIDE),
+    // "YES".equals(values[4]) ? OnOffType.ON : OnOffType.OFF);
+    //
+    // updateState(new ChannelUID(getThing().getUID(), CHANNEL_COOL_SETPOINT),
+    // new DecimalType(Double.parseDouble(values[6])));
+    // updateState(new ChannelUID(getThing().getUID(), CHANNEL_HEAT_SETPOINT),
+    // new DecimalType(Double.parseDouble(values[7])));
+    // updateState(new ChannelUID(getThing().getUID(), CHANNEL_COMPRESSOR_MODE), new StringType(values[8]));
+    // updateState(new ChannelUID(getThing().getUID(), CHANNEL_COMPRESSOR_STAGE), new StringType(values[9]));
+    //
+    // result = sendCommand("RAOS1"); // RAOS1:15-10-15,17:27:53,0:00,0:0:00,TH3,DISABLED
+    // logger.trace("RAOS1: {}", result);
+    // values = result.substring(6).split(",");
+    // String override[] = values[2].split(":");
+    // updateState(new ChannelUID(getThing().getUID(), CHANNEL_OVERRIDE_TIMER),
+    // new DecimalType(Integer.parseInt(override[0]) * 60 + Integer.parseInt(override[1])));
+    //
+    // result = sendCommand("RAAS1"); // RAAS1:YES,NO,NO,NO,NO,YES,NO,NO,NO,NO,NO,YES,NO,NO,NO,NO
+    // logger.trace("RAAS1: {}", result);
+    // values = result.substring(6).split(",");
+    // updateState(new ChannelUID(getThing().getUID(), CHANNEL_LED1),
+    // "YES".equals(values[14]) ? OnOffType.ON : OnOffType.OFF);
+    // updateState(new ChannelUID(getThing().getUID(), CHANNEL_LED2),
+    // "YES".equals(values[15]) ? OnOffType.ON : OnOffType.OFF);
+    //
+    // /*
+    // * <channel id="cool_setpoint" typeId="setpoint"/>
+    // * <channel id="heat_setpoint" typeId="setpoint"/>
+    // * <channel id="mode" typeId="mode"/>
+    // * <channel id="indoor_temp" typeId="current_temp"/>
+    // * <channel id="outdoor_temp" typeId="current_temp"/>
+    // * <channel id="compressor_mode" typeId="compressor_mode"/>
+    // * <channel id="compressor_stage" typeId="compressor_stage"/>
+    // */
+    //
+    // // TODO Auto-generated method stub
+    //
+    // }
 
     @Override
     public void handleCommand(ChannelUID channelUID, Command command) {
-        if (channelUID.getId().equals(CHANNEL_COOL_SETPOINT)) {
-            // TODO: handle command
-
-            // Note: if communication with thing fails for some reason,
-            // indicate that by setting the status with detail information
-            // updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR,
-            // "Could not control device at IP address x.x.x.x");
-        }
-
         if (channelUID.getId().equals(CHANNEL_MODE)) {
             ThermostatModeType modeCommand = ThermostatModeType.valueOf(command.toString());
             switch (modeCommand) {
@@ -155,8 +233,7 @@ public class NetworkThermostatHandler extends BaseThingHandler {
                     logger.info("Updating mode: {}", sendCommand("WMS1DO"));
             }
             updateThermostatStatus();
-        }
-        if (channelUID.getId().equals(CHANNEL_FAN_MODE)) {
+        } else if (channelUID.getId().equals(CHANNEL_FAN_MODE)) {
             FanModeType modeCommand = FanModeType.valueOf(command.toString());
             switch (modeCommand) {
                 case AUTO:
@@ -169,18 +246,15 @@ public class NetworkThermostatHandler extends BaseThingHandler {
                     sendCommand("WMS1DR");
             }
             updateThermostatStatus();
-        }
-        if (channelUID.getId().equals(CHANNEL_COOL_SETPOINT)) {
+        } else if (channelUID.getId().equals(CHANNEL_COOL_SETPOINT)) {
             logger.info("Override cool setpoint to {}: {}", command.toString(),
                     sendCommand("WOS1DX,X," + command.toString() + ",X"));
             updateThermostatStatus();
-        }
-        if (channelUID.getId().equals(CHANNEL_HEAT_SETPOINT)) {
+        } else if (channelUID.getId().equals(CHANNEL_HEAT_SETPOINT)) {
             logger.info("Override heat setpoint to {}: {}", command.toString(),
                     sendCommand("WOS1DX,X,X," + command.toString()));
             updateThermostatStatus();
-        }
-        if (channelUID.getId().equals(CHANNEL_OVERRIDE) && command.equals(OnOffType.OFF)) {
+        } else if (channelUID.getId().equals(CHANNEL_OVERRIDE) && command.equals(OnOffType.OFF)) {
             logger.info("Cancel override: {}", sendCommand("WOR1D0:00"));
             updateThermostatStatus();
         }
@@ -208,20 +282,10 @@ public class NetworkThermostatHandler extends BaseThingHandler {
                 return;
             }
         }
-        connect();
         super.initialize();
+        updateStatus(ThingStatus.INITIALIZING);
         onUpdate();
-
-        // TODO: Initialize the thing. If done set status to ONLINE to indicate proper working.
-        // Long running initialization should be done asynchronously in background.
-        updateStatus(ThingStatus.ONLINE);
-
-        // Note: When initialization can NOT be done set the status with more details for further
-        // analysis. See also class ThingStatusDetail for all available status details.
-        // Add a description to give user information to understand why thing does not work
-        // as expected. E.g.
-        // updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR,
-        // "Can not access device as username and/or password are invalid");
+        threadPool.execute(thermostatReader);
     }
 
     private void connect() {
@@ -229,23 +293,30 @@ public class NetworkThermostatHandler extends BaseThingHandler {
             socket = new Socket(ipAddress, port);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new PrintWriter(socket.getOutputStream(), true);
+            logger.info("Logging in...");
+            sendCommand("WML1Dadmin,netx");
+            updateStatus(ThingStatus.ONLINE);
         } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            logger.debug("Failed to connect to thermostat {} at {}:{}", getThing().getUID(), ipAddress, port);
+            close();
         }
-        logger.info("Logging in: {}", sendCommand("WML1Dadmin,netx"));
     }
 
-    private String sendCommand(String command) {
-        out.println(command);
+    private void close() {
         try {
-            return in.readLine();
+            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR);
+            socket.close();
         } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            return null;
+            logger.debug("Failed to disconnect to thermostat {} at {}:{}", getThing().getUID(), ipAddress, port);
         }
+    }
 
+    private boolean sendCommand(String command) {
+        if (out == null) {
+            return false;
+        }
+        out.println(command);
+        return true;
     }
 
     private synchronized void onUpdate() {
@@ -257,7 +328,8 @@ public class NetworkThermostatHandler extends BaseThingHandler {
             if (refreshConfig != null) {
                 refreshInterval = ((BigDecimal) refreshConfig).intValue();
             }
-            pollingJob = scheduler.scheduleAtFixedRate(pollingRunnable, 0, refreshInterval, TimeUnit.SECONDS);
+            pollingJob = scheduler.scheduleAtFixedRate(pollingRunnable, (int) (refreshInterval * Math.random()),
+                    refreshInterval, TimeUnit.SECONDS);
         }
     }
 }
